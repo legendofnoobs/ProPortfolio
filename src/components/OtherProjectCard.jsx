664599ce2f70c812a05b8ca341efab263e5bdd83
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-02-10 22:16:28 +0200

    test images

--- src/components/OtherProjectCard.jsx
+++ src/components/OtherProjectCard.jsx
@@ -51,7 +51,7 @@ const OtherProjectCard = (props) => {
 					</div>
 				</div>
 				<div className="rounded-xl z-20 relative">
-					<img src={props.imgURL} alt={props.title} className="rounded-xl w-full h-auto relative top-16 group-hover:top-0 transition-all duration-300 max-[768px]:top-0 aspect-video" loading="lazy"
+					<img src={props.imgURL} alt={props.title} className="rounded-xl w-full h-auto relative top-16 group-hover:top-0 transition-all duration-300 max-[768px]:top-0 aspect-video"
 						width={16}
 						height={9} />
 				</div>
