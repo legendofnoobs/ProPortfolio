667f8d111705617458d0c3f2eade4937708b5393
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-02-23 00:49:45 +0200

    some edits in the other work section from flex to grid

--- src/components/OtherWork.jsx
+++ src/components/OtherWork.jsx
@@ -1,32 +1,3 @@
-// import OtherProjectCard from "../components/OtherProjectCard";
-// import { OtherProjectsData } from "../data";
-
-// const OtherWork = () => {
-// 	return (
-// 		<>
-// 			<h1 className="text-4xl">⬇️ <span style={{ textShadow: "0px 0px 10px white" }}>Other Work</span></h1>
-// 			<div className="flex flex-wrap items-center justify-center gap-5 w-full">
-// 				{OtherProjectsData.map((project) => (
-// 					<OtherProjectCard
-// 						key={project.id}
-// 						title={project.title}
-// 						description={project.description}
-// 						usedTech={project.usedTech}
-// 						live={project.live}
-// 						gitHUB={project.gitHUB}
-// 						imgURL={project.imgURL}
-// 						id={project.id}
-// 						hover={project.hover}
-// 						gallery={project.gallery}
-// 					/>
-// 				))}
-// 			</div>
-// 		</>
-// 	)
-// }
-
-// export default OtherWork
-
 import { useState } from "react";
 import OtherProjectCard from "../components/OtherProjectCard";
 import { OtherProjectsData } from "../data";
@@ -37,7 +8,7 @@ const OtherWork = () => {
 	return (
 		<>
 			<h1 className="text-4xl">⬇️ <span style={{ textShadow: "0px 0px 10px white" }}>Other Work</span></h1>
-			<div className="flex flex-wrap items-center justify-center gap-5 w-full">
+			<div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-5 w-full">
 				{OtherProjectsData.map((project) => (
 					<div
 						key={project.id}
